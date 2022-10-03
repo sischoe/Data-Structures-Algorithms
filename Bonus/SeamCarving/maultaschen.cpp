2272a5dedc75e1a72859ae2dd83cfa55e3352839
sischoe <106981893+sischoe@users.noreply.github.com>
2022-10-04 00:55:46 +0200

    Delete maultaschen.cpp

--- Bonus/SeamCarving/maultaschen.cpp
+++
@@ -1,3 +0,0 @@
-Maultaschen sind den Ravioli ähnlich, aber die Füllung aus Hackfleisch, Spinat, Eiern, Paniermehl und Muskat schmeckt ganz anders.
-  
-  https://www.swissmilk.ch/de/rezepte-kochideen/rezepte/SM2021_DIVE_84/maultaschen/
